ae8721418598dadf6d20e0bce33130fd65f34a6f
Elis Mattosinho <eliscmattosinho@gmail.com>
2025-01-30 20:39:32 -0300

    feat: capa de introdução do sobre

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Sobre from "./components/Sobre";
 import "./styles/App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <div id="main">
       <Header /> {}
+      <Sobre /> {}
       </div>
     </div>
   );
