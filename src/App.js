96274fe62643f73d05570b6487f0945797313ce5
Elis Mattosinho <eliscmattosinho@gmail.com>
2025-01-30 20:30:08 -0300

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <div id="main">
+      <Header /> {}
       </div>
     </div>
   );
