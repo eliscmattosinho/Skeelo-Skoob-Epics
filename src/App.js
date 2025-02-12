c1cf92f8beda788e9583b5007444d8262e428913
Elis Mattosinho <eliscmattosinho@gmail.com>
2025-02-12 20:18:55 -0300

    refactor: extração do board de desenvolvimento

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./styles/App.css";
 
-import Building from "./components/Building";
-import Boards from "./components/Boards";
-import LandingPage from "./pages/LandingPage.js"; // Novo componente que reúne a LP
+import LandingPage from "./pages/LandingPage.js";
 
 function App() {
   return (
@@ -14,8 +12,6 @@ function App() {
         <div id="main">
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/Skeelo-Skoob-Epics" element={<Building />} />
-            <Route path="/boards" element={<Boards />} />
           </Routes>
         </div>
       </div>
